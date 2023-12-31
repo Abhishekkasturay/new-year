bb14100b19f5276d1a1ce48a60581d2c3cba6051
Abhishekkasturay <136403845+Abhishekkasturay@users.noreply.github.com>
2023-12-31 23:14:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const box2 = document.querySelector(".box2");
 const btn = document.querySelector(".btn");
 
 btn.addEventListener("click", function () {
-  box1.classList.toggle("set");
-  box2.classList.toggle("set2");
+  box1.classList.toggle("active");
+  box2.classList.toggle("active");
+
+  if (box2.classList.contains("active")) {
+    btn.style.visibility = "hidden";
+  } else {
+    btn.style.visibility = "visible";
+  }
 });
+
